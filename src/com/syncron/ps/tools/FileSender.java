b2db785af2957d4c83a5ce42e531864a5767df14
micsie <michal.siedlaczek@syncron.com>
2013-12-09 14:55:51 +0100

    File sender interface and SFTP implementation

---
+++ src/com/syncron/ps/tools/FileSender.java
@@ -0,0 +1,21 @@
+package com.syncron.ps.tools;
+
+import java.nio.file.Path;
+
+/**
+ * 
+ * An interface for transferring files to a remote location.
+ * @author micsie
+ *
+ */
+public interface FileSender {
+	
+	/**
+	 * Transfers a file to a remote location.
+	 * @param path			A path to a local file.
+	 * @param remoteDir		A directory on the remote server.
+	 * @throws TransferFailedException
+	 */
+	public void transferFile(Path path, String remoteDir) throws TransferFailedException;
+	
+}
