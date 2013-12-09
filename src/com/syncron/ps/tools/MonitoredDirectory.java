c9085237ab98d3111e6a2f30ab1a9327a6c879f6
micsie <michal.siedlaczek@syncron.com>
2013-12-09 14:57:52 +0100

    MonitoredDirectory class created
    MonitoredDirectory maps a local dir to a remote one and, at this point,
    contains file masks.

---
+++ src/com/syncron/ps/tools/MonitoredDirectory.java
@@ -0,0 +1,64 @@
+package com.syncron.ps.tools;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 
+ * Stores information about transferring files
+ * from a local directory to a remote one.
+ * @author micsie
+ *
+ */
+public class MonitoredDirectory {
+	
+	private String localDirectory;
+	private String remoteDirectory;
+	
+	/**
+	 * A list of regular expressions: the files with names
+	 * matching any of those should be sent.
+	 */
+	List<String> filterMasks;
+	
+	public MonitoredDirectory(String localDirectory, String remoteDirectory) {
+		this.localDirectory = localDirectory;
+		this.remoteDirectory = remoteDirectory;
+		filterMasks = new LinkedList<String>();
+	}
+	
+	/**
+	 * Adds a filter mask for a file name.
+	 * @param mask	regular expression
+	 */
+	public void registerMask(String mask) {
+		filterMasks.add(mask);
+	}
+	
+	/**
+	 * Checks if fileName matches any filter mask.
+	 * @param fileName	a string in question
+	 * @return	true iff <b>fileName</b> matches to any filter mask
+	 */
+	public boolean filter(String fileName) {
+		
+		boolean result = false;
+		
+		for (String mask : filterMasks) {
+			if (fileName.matches(mask)) {
+				return true;
+			}
+		}
+		
+		return result;
+	}
+	
+	public String getLocalDirectory() {
+		return localDirectory;
+	}
+	
+	public String getRemoteDirectory() {
+		return remoteDirectory;
+	}
+	
+}
